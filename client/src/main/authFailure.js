db1d7f32cab3deb2f53db85790ba51ad5f92cd1c
Shinkyuuu <codyspark@gmail.com>
2026-07-13 11:28:40 -0700

    Updated README and added licensing

--- client/src/main/authFailure.js
+++ client/src/main/authFailure.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2026 Cody Park
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // 4001 (not in voice channel) means the token is still valid - the user is just
 // not currently in the tracked VC (and might join it later) - so it must not be
 // treated the same as an actually-bad token (invalid/expired: 4002/4003/4008),
